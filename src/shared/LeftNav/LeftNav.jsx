b54bb4b49fac2699e157bd0f00b5c8dcaaa0b492
Mahadi90 <mahadimhs787@gmail.com>
2023-04-19 02:15:33 +0600

    righnav somthing added

--- src/shared/LeftNav/LeftNav.jsx
+++ src/shared/LeftNav/LeftNav.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h3>This is lefft side nav</h3>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
+  return (
+    <div className="border p-3">
+      <h3>All Category</h3>
+      <div className="ms-4">
+        {
+            categories.map(category => <p
+            key={category.id}
+            >
+     <Link className="text-decoration-none">{category.name}</Link>
+            </p>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
